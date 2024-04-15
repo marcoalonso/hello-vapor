155e261a699f4e2a2c6bb9ee12c0280a93df08f9
Marco Alonso <marcoalonsorota@hotmail.com>
2024-04-15 15:50:23 -0600

    Basic Routes and Route Parameters

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,28 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    //movies/action
+    app.get("movies", "action") { req async -> String in
+        "Movies of action"
+    }
+    
+    // route parameters/dynamic parameters
+    /// http://127.0.0.1:8080/movies/drama
+    
+    app.get("movies", ":genre") { req async  throws -> String in
+        guard let genre = req.parameters.get("genre") else {
+            throw Abort(.badRequest)
+        }
+        return "All movies of genre: \(genre)"
+    }
+    
+    // /movies/action/2024
+    app.get("movies", ":genre", ":year") { req async  throws -> String in
+        guard let genre = req.parameters.get("genre"),
+        let year = req.parameters.get("year") else {
+            throw Abort(.badRequest)
+        }
+        return "All movies of genre: \(genre) for year \(year)"
+    }
 }
